3967f53156cb8a5b6b033226daefc34f43f8f8f8
ILIA <iliaborouji@gmail.com>
2026-06-30 10:52:46 +0330

    change main

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from redis.asyncio import Redis
 
 from config import Config
 
+redis = Redis(
+    Config.REDIS_HOST,
+    Config.REDIS_PORT,
+    Config.REDIS_DB,
+    encoding="utf-8",
+    decode_responses=True
+)
+storage = RedisStorage(redis=redis)
+    
+bot = Bot(token=Config.BOT_TOKEN)
+dp = Dispatcher(storage=storage)
+    
 async def main():
-    redis = Redis(
-        Config.REDIS_HOST,
-        Config.REDIS_PORT,
-        Config.REDIS_DB,
-        encoding="utf-8",
-        decode_responses=True
-    )
-    storage = RedisStorage(redis=redis)
-    
-    bot = Bot(token=Config.BOT_TOKEN)
-    dp = Dispatcher(storage=storage)
-    
     await dp.start_polling(bot)
     
 if __name__=='__main__':
